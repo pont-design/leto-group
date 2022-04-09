971e0a9b0cb711c6031c8f52f6e5c6aaed847b36
dosjcq <nagibin.artyom1999@gmail.com>
2022-04-09 19:46:51 +0300

    feat: add custom accordion component

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,9 @@
+import '../styles/base.scss';
+import '../styles/variables.scss';
+import '../styles/global.scss';
+import '../styles/productCard.scss';
+import '../styles/CustomAccordeonStyles.scss';
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
