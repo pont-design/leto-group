94a00a14aa827ab9f834fc55abee524858532dd9
dosjcq <nagibin.artyom1999@gmail.com>
2022-04-10 14:53:16 +0300

    feat: add styles for accordion

--- components/CustomAccordeon.js
+++ components/CustomAccordeon.js
@@ -1,15 +1,29 @@
 import React, { useState } from 'react';
 
+import accordionArrow from '../public/images/customAccordion/accordion-arrow.svg';
+
 export const CustomAccordion = ({ title, content }) => {
   const [isActive, setIsActive] = useState(false);
 
   return (
     <div className="accordion-item">
       <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
-        <div>{title}</div>
-        <div>{isActive ? '-' : '+'}</div>
+        <div>
+          <p className="card-accard">{title}</p>
+        </div>
+        <div>
+          <img
+            className={isActive ? 'accordion-arrow' : ''}
+            src={accordionArrow.src}
+            alt="arrow"
+          />
+        </div>
       </div>
-      {isActive && <div className="accordion-content">{content}</div>}
+      {isActive && (
+        <div className="accordion-content">
+          <p className="text1">{content}</p>
+        </div>
+      )}
     </div>
   );
 };
