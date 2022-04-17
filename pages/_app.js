da3e4896092945b9719492f641dd724c8c806a61
dosjcq <nagibin.artyom1999@gmail.com>
2022-04-17 18:26:22 +0300

    feat: start about page

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,27 @@
-import { Header } from "../Components/Common/Header";
-import { Footer } from "../components/Common/Footer";
+import { Header } from '../Components/Common/Header';
+import { Footer } from '../components/Common/Footer';
 
 import '../styles/base.scss';
 import '../styles/global.scss';
 
 import '../styles/ProductCard.scss';
 import '../styles/Catalog.scss';
+import '../styles/aboutStyles.scss';
 
 import '../styles/CustomAccordeonStyles.scss';
 import '../styles/CustomButton.scss';
 import '../styles/CustomDropDown.scss';
 import '../styles/CustomFiltersStyles.scss';
-import '../styles/CustomLanguageSwitcher.scss'
+import '../styles/CustomLanguageSwitcher.scss';
 import '../styles/CustomLink.scss';
-import '../styles/CustomOutlinedButton.scss'
-import '../styles/CustomRadioButtonStyle.scss'
+import '../styles/CustomOutlinedButton.scss';
+import '../styles/CustomRadioButtonStyle.scss';
 
-import '../styles/Footer.scss'
-import '../styles/Header.scss'
+import '../styles/Footer.scss';
+import '../styles/Header.scss';
 
-import '../styles/ProductCard.scss'
-import '../styles/Technologies.scss'
+import '../styles/ProductCard.scss';
+import '../styles/Technologies.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
---
+++ pages/about.js
@@ -0,0 +1,68 @@
+import mainImage from '../public/images/AboutPage/about-main.jpg';
+import ideasImage from '../public/images/AboutPage/ideas.jpg';
+
+export default function about() {
+  const companyIdeas = ['Развитие', 'Партнерство', 'Качество', 'Инновации'];
+
+  const getCompanyList = () => {
+    return (
+      <ul className="about__ideas-list">
+        {companyIdeas.map((el, idx) => (
+          <li className="about__ideas-item" key={el}>
+            <p className="about__ideas-item-idx">0{idx + 1}</p>
+            <p className="h0">{el}</p>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  return (
+    <div className="about">
+      <div className="about__heading">
+        <h1 className="about__heading-text">
+          Входим в топ-10 производителей яиц
+        </h1>
+        <img className="about__heading-img" src={mainImage.src} alt="view" />
+      </div>
+      <div className="about__eco-products">
+        <h2 className="about__eco-products-heading">
+          Только экологически чистые и здоровые продукты питания
+        </h2>
+        <p className="caption-1 about__eco-products-caption">
+          Наша главная цель — укрепление позиций компании как ведущего
+          производителя высококачественной брендированной продукции, а также
+          усиления вертикальной интеграции.
+        </p>
+      </div>
+      <div className="about__numbers">
+        <div className="about__numbers-packaging">
+          <p className="text-numbers">70 000 000 +</p>
+          <p className="text-1">Продаём упаковок в год</p>
+        </div>
+        <div className="about__numbers-technology">
+          <p className="text-numbers">SANOVO</p>
+          <p className="text-1">
+            Завод оборудован современной техникой датской компании
+          </p>
+        </div>
+        <div className="about__numbers-square">
+          <p className="text-numbers about__numbers-square-text">3000 Га</p>
+          <p className="text-1">Рабочая площадь преприятия</p>
+        </div>
+      </div>
+      <div className="about__ideas">
+        <div className="about__ideas-text">
+          {getCompanyList()}
+          <p className="text-1">
+            Наша команда имеет высокий уровень компетенции и дальше надо что-то
+            дописать
+          </p>
+        </div>
+        <div className="about__ideas-img">
+          <img src={ideasImage.src} alt="Ideas" />
+        </div>
+      </div>
+    </div>
+  );
+}
