bcf3724d43be5dd90a3b9d0d10da9bdbc10b2184
dosjcq <nagibin.artyom1999@gmail.com>
2022-05-05 21:05:43 +0300

    fix: fix filter slider

--- components/UI/CustomFilters/CustomFilters.js
+++ components/UI/CustomFilters/CustomFilters.js
@@ -4,21 +4,20 @@ import { CustomDropDown } from '../customDropDown/CustomDropDown';
 
 import { filters } from '../../../assets/mockFilters';
 
-import { amountDeclination } from "../../../utils/amountDeclination";
-import { SliderWrapper } from "../../SliderWrapper/SliderWrapper";
+import { amountDeclination } from '../../../utils/amountDeclination';
+import { SliderWrapper } from '../../SliderWrapper/SliderWrapper';
 
 export const CustomFilters = ({
   productWordsDeclination,
   filterValue,
   setFilterValue,
-  filteredValue
+  filteredValue,
 }) => {
-
   const factorySliderBreakPoint = {
     gapxl: 1,
     gapXs: 16,
     gapMd: 16,
-    gapXl: 16
+    gapXl: 16,
   };
 
   const filtersElems = filters.map((el) => (
@@ -29,25 +28,32 @@ export const CustomFilters = ({
       filterValue={filterValue}
       setFilterValue={setFilterValue}
     />
-  ))
+  ));
 
   const getAmountDeclination = (words) => {
-    return `${words.length} ${amountDeclination(words.length, productWordsDeclination)}`;
+    return `${words.length} ${amountDeclination(
+      words.length,
+      productWordsDeclination
+    )}`;
   };
 
   return (
     <div className="filters-container">
-      <div className="filter-container__filters">
-        <p className="tag-text">Фильтры</p>
-        <div className="filter-container-swiper-quality">
-          <SliderWrapper
+      <div className="test2">
+        <div className="test3">
+          <div className="filter-container__filters">
+            <p className="tag-text">Фильтры</p>
+            <div className="filter-container-swiper-quality">
+              {/* <SliderWrapper
             listOfElems={filtersElems}
             breakpointsObj={factorySliderBreakPoint}
             swiperWrapperStyle='filter-container-swiper-quality'
-          />
+          /> */}
+              <div className="test1">{filtersElems}</div>
+            </div>
+          </div>
         </div>
       </div>
-
       <p className="tag-text filter-container__amount">
         {getAmountDeclination(filteredValue)}
       </p>
